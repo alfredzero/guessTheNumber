7f875fc6ac387729e890399240fd697cdb96052f
alfredzero <amezcuaalfredo81@gmail.com>
2024-04-30 21:09:54 -0700

    Initial Commit

---
+++ numberGuesser/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  numberGuesser
+//
+//  Created by Alfredo Amezcua on 4/30/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
